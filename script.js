e9291aea3bae6b4043f6f3987c82bec875d6dee2
annguyen <thienanvsfamily123@gmail.com>
2023-05-27 17:48:16 +0700

    change text

---
+++ script.js
@@ -0,0 +1,17 @@
+function copyMenu( ) {
+    //copy inside .dpt-cat to departments
+    var dptCategory = document.querySelector('.dpt-cat');
+    var dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    //copy inside nav to nav
+    var mainNav = document.querySelector('.header-nav nav');
+    var navPLace = document.querySelector('.off-canvas nav');
+    navPLace.innerHTML = mainNav.innerHTML;
+
+    //copy .header-top .wrapper to .thetop-nav
+    var topNav = document.querySelector('.header-top .wrapper');
+    var topPlace = document.querySelector('.off-canvas .thetop-nav');
+    topPlace.innerHTML = topNav.innerHTML;
+}
+copyMenu();  
